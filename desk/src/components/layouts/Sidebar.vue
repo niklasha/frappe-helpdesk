fda7891b2aa9bb6af88def106ad6833bdd8b7ad9
Niklas Hallqvist <niklas@appli.se>
2026-08-29 12:44:55 +0000

    fix onboarding completion for administrators

--- desk/src/components/layouts/Sidebar.vue
+++ desk/src/components/layouts/Sidebar.vue
@@ -219,7 +219,7 @@ const showOnboardingBanner = computed(() => {
   return (
     !isCustomerPortal.value &&
     !isOnboardingStepsCompleted.value &&
-    authStore.isManager
+    (authStore.isManager || authStore.isAdmin)
   );
 });
 
@@ -482,7 +482,7 @@ async function getGeneralCategory() {
 }
 
 function setUpOnboarding() {
-  if (!authStore.isManager) return;
+  if (!authStore.isManager && !authStore.isAdmin) return;
   setUp(steps);
   useShortcut({ key: "h", meta: true }, () => {
     showHelpModal.value = !showHelpModal.value;
