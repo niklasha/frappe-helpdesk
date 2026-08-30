675ec1fb029f4d527b60a49b540374c94d1ed846
Niklas Hallqvist <niklas@appli.se>
2026-08-30 13:51:07 +0000

    feat: let authorized users administer the knowledge library

--- helpdesk/api/knowledge_library.py
+++ helpdesk/api/knowledge_library.py
@@ -1,7 +1,8 @@
 import frappe
+from frappe import _
 from frappe.utils import cint
 
-from helpdesk.utils import agent_only
+from helpdesk.utils import agent_only, is_admin
 
 
 @frappe.whitelist()
@@ -22,3 +23,28 @@ def search_knowledge(query, limit=5):
         order_by="modified desc",
         limit_page_length=cint(limit) or 5,
     )
+
+
+def _require_knowledge_admin():
+    """Curating the library is an administrative act, not an agent action."""
+    if not is_admin():
+        frappe.throw(
+            _("You are not permitted to administer the knowledge library."),
+            frappe.PermissionError,
+        )
+
+
+@frappe.whitelist(methods=["POST"])
+@agent_only
+def upsert_knowledge_article(title, content, category=None, article=None):
+    """Create or update a knowledge-library article as an authorized user."""
+    _require_knowledge_admin()
+    doc = (
+        frappe.get_doc("HD Article", article) if article else frappe.new_doc("HD Article")
+    )
+    doc.title = title
+    doc.content = content
+    if category:
+        doc.category = category
+    doc.save(ignore_permissions=True)
+    return doc.as_dict()
